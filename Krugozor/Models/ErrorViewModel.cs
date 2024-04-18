b2bbd4d8c64d090560106b5cc6510cfb020fcd5f
KonstantinM <forever_021@mail.ru>
2024-04-18 13:04:56 +0300

    Перенос проекта на ASP.NET MVC

---
+++ Krugozor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Krugozor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
